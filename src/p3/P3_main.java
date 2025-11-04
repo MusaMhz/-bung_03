8f037d0927fe628f342102a69192409b3179c84f
Musawer Mohibzada <musawer@MacBook-Pro-von-Musawer.local>
2025-11-04 11:24:00 +0100

    P

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -1,5 +1,20 @@
 package p3;
 
 public class P3_main {
-
+	public static void main (String[] args) { 
+		int a=-7;							// Nummer 1)
+		System.out.println(Math.abs(a));
+		
+		int b=2;							// Nummer 2)
+		System.out.println("Das Quadrat der Zahl"+b+"ist gleich:"+(b*b));
+		
+		int c=3; // Nummer 3)
+		if (c% 2==0) {
+			System.out.println("Die Zahl  "+c+" ist gerade.");
+		} else {
+			System.out.println("Die Zahl "+c+"ist ungerade");
+		}
+		
+		
+	}
 }
